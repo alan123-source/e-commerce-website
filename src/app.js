0174ec61931fb097e9a2282538e168332f844cfe
Alan paul john <alanpauljohn385@gmail.com>
2026-05-30 22:18:44 +0530

    going to start payment  features

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ import userRoutes from "./routes/userRoutes.js";
 import { protect } from './middleware/authmiddleware.js';
 import productRoutes from "./routes/productRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
-//import paymentRoutes from "./routes/paymentRoutes.js";
+import paymentRoutes from "./routes/paymentRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import recommendationRoutes from "./routes/recommendationRoutes.js";
@@ -56,7 +56,7 @@ app.get("/api/protected",protect,(req,res)=>{
 app.use("/api/users",userRoutes);
 app.use("/api/products",productRoutes);
 app.use("/api/orders",orderRoutes);
-//app.use("/api/payments",paymentRoutes);
+app.use("/api/payments",paymentRoutes);
 app.use("/api/cart",cartRoutes);
 app.use("/api/admin",adminRoutes);
 app.use("/api/recommendations",recommendationRoutes);
--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,7 +1,7 @@
-// import express from "express";
-// import { createPaymentIntent,refundPayment } from "../controllers/paymentController.js";
-// import {protect,admin} from "../middleware/authmiddleware.js";
-// const router=express.Router();
-// router.post("/create",protect,createPaymentIntent);
-// router.post("/refund",protect,admin,refundPayment);
-// export default router;
+import express from "express";
+import { createPaymentIntent,refundPayment } from "../controllers/paymentController.js";
+import {protect,admin} from "../middleware/authmiddleware.js";
+const router=express.Router();
+router.post("/create",protect,createPaymentIntent);
+router.post("/refund",protect,admin,refundPayment);
+export default router;
